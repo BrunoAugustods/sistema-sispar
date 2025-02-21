8fe84006dc24d14be5c599695250803f74661c01
BrunoAugustods <brunoaugustodssilva@gmail.com>
2025-02-21 14:03:15 -0300

    primeiro envio, paginas login navbar reembolosos 90% prontas, iniciando página solicitação

---
+++ src/components/login/Login.jsx
@@ -0,0 +1,36 @@
+import { useNavigate } from "react-router-dom";
+import Capa from "../../assets/Tela Login/imagem tela de login.png";
+import Logo from "../../assets/Tela Login/logo-ws.png";
+import styles from "./Login.module.scss";
+
+function Login() {
+    const navigate = useNavigate()
+    const botaoEntrar = () => {
+        navigate("/reembolsos")
+    }
+    return (
+        <main className={styles.mainLogin}>
+            <section className={styles.containerImagem}></section>
+
+            <section className={styles.containerLogin}>
+                <div className={styles.boxLogo}>
+                    <img src={Logo} alt="Logo da Wilson sons" />
+                    <h1>Boas vindas ao Novo Portal SISPAR</h1>
+                    <p>Sistema de Emissão de Boletos e Parcelamento</p>
+                </div>
+
+                <form action="">
+                    <input type="email" name="email" id="email" placeholder="Email" />
+                    <input type="password" name="password" id="password" placeholder="Senha" />
+                    <p>Esqueci minha senha</p>
+                    <div className={styles.boxBotoes}>
+                        <button onClick={botaoEntrar}>Entrar</button>
+                        <button>Criar conta</button>
+                    </div>
+                </form>
+            </section>
+        </main>
+    );
+}
+
+export default Login;
